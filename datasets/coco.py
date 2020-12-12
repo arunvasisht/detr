60b70c397ad8bb60f0195d72747391910c292e4d
arunvasisht <mystikalarun@gmail.com>
2020-12-13 00:14:23 +0530

    restored normalize

--- datasets/coco.py
+++ datasets/coco.py
@@ -146,7 +146,7 @@ def make_coco_transforms(image_set):
         # noise augmentation code
         return T.Compose([
             T.RandomResize([800], max_size=1333),
-            T.ToTensor(),
+            normalize,
             T.AddGaussianNoise(0.0, 0.1)
         ])
 
