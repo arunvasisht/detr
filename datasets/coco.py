576325d137e7fed3e84222840877d78860435297
arunvasisht <mystikalarun@gmail.com>
2020-12-19 17:34:20 +0530

    switched to RandomErasing

--- datasets/coco.py
+++ datasets/coco.py
@@ -145,6 +145,7 @@ def make_coco_transforms(image_set):
 
         # noise augmentation code
         return T.Compose([
+            T.RandomResize([800], max_size=1333),
             T.RandomSizeCrop(384, 600),
             normalize
         ])
@@ -152,7 +153,8 @@ def make_coco_transforms(image_set):
     if image_set == 'val':
         return T.Compose([
             T.RandomResize([800], max_size=1333),
-            normalize
+            normalize,
+            T.RandomErasing()
         ])
 
     raise ValueError(f'unknown {image_set}')
