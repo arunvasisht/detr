f0122e411414b8995be4db30d848f51bb395a949
arunvasisht <mystikalarun@gmail.com>
2020-12-20 01:53:43 +0530

    Random Composition of Transforms

--- datasets/coco.py
+++ datasets/coco.py
@@ -5,6 +5,7 @@ COCO dataset which returns image_id for evaluation.
 Mostly copy-paste from https://github.com/pytorch/vision/blob/13b35ff/references/detection/coco_utils.py
 """
 from pathlib import Path
+from numpy.core.fromnumeric import mean
 
 import torch
 import torch.utils.data
@@ -145,7 +146,24 @@ def make_coco_transforms(image_set):
         # noise augmentation code
         return T.Compose([
             T.RandomResize([800], max_size=1333),
-            T.ColorJitter(brightness=[0.3,0.7], hue=[0.3,0.5],contrast=[0.3,0.5],saturation=[0.3,0.7]),
+            T.Compose([
+                T.RandomSelect(
+                    T.RandomResize([400, 500, 600]),
+                    T.RandomSizeCrop(384, 600),
+                    p=0.5
+                ),
+                T.RandomSelect(
+                    T.AddGaussianNoise(mean=0.0,std=0.3),
+                    T.AddGaussianNoise(mean=0, std=0.7),
+                    p=0.6
+                ),
+                T.RandomSelect(
+                    T.ColorJitter(brightness=0.5),
+                    T.ColorJitter(contrast=0.5),
+                    p=0.3
+                )
+            ])
+            ,
             normalize
         ])
 
